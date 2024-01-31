ceb02b39f48062616ae15efffe1547b159a74944
ThibSara <sara.thibierge@gmail.com>
2024-01-31 16:14:01 +0100

    Add sound buttons to App.js

--- App.js
+++ App.js
@@ -1,9 +1,29 @@
 import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import  SoundButton  from './src/utils/SoundButton';
+
 
 export default function App() {
+  
   return (
-    <View>
-      <Text> Hello there ! </Text>
+    <View style={styles.container}>
+      <Text> Hello there !! </Text>
+      <SoundButton soundKey="do"/>
+      <SoundButton soundKey="re"/>
+      <SoundButton soundKey="mi"/>
+      <SoundButton soundKey="fa"/>
+      <SoundButton soundKey="sol"/>
+      <SoundButton soundKey="la"/>
+      <SoundButton soundKey="si"/>
+      <SoundButton soundKey="do_octave"/>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
