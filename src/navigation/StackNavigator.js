73e4e947868b13a5c846e5b0bba57e870ead1747
ThibSara <sara.thibierge@gmail.com>
2024-02-12 14:11:03 +0100

    Add LogScreen to StackNavigator

--- src/navigation/StackNavigator.js
+++ src/navigation/StackNavigator.js
@@ -11,9 +11,9 @@ const Stack = createStackNavigator();
 const StackNavigator = () => {
   return (
     <Stack.Navigator>
+      <Stack.Screen name="LogScreen" component={LogScreen} options={{ headerShown: false }} />
       <Stack.Screen name = "DragDrop" component={DragDropScreen} options={{ headerShown: false }} />
       <Stack.Screen name="NotesGame" component={NotesGame} options={{ headerShown: false }} />
-      <Stack.Screen name="LogScreen" component={LogScreen} options={{ headerShown: false }} />
       <Stack.Screen name="CreateAccountScreen" component={CreateAccountScreen} options={{ headerShown: false }} />
       <Stack.Screen name="HomeScreen" component={HomeScreen} options={{ headerShown: false }} />
 
