60caf1f2ee1a5c4c428aa6d7b644a4dc443a9af5
ThibSara <sara.thibierge@gmail.com>
2024-02-20 21:57:49 +0100

    Refactor navigation and styling in StackNavigator.js and CreateAccountScreen.js

--- src/navigation/StackNavigator.js
+++ src/navigation/StackNavigator.js
@@ -11,8 +11,8 @@ const Stack = createStackNavigator();
 const StackNavigator = () => {
   return (
     <Stack.Navigator>
-      <Stack.Screen name="LogScreen" component={LogScreen} options={{ headerShown: false }} />
       <Stack.Screen name = "DragDrop" component={DragDropScreen} options={{ headerShown: false }} />
+      <Stack.Screen name="LogScreen" component={LogScreen} options={{ headerShown: false }} />
       <Stack.Screen name="NotesGame" component={NotesGame} options={{ headerShown: false }} />
       <Stack.Screen name="CreateAccountScreen" component={CreateAccountScreen} options={{ headerShown: false }} />
       <Stack.Screen name="HomeScreen" component={HomeScreen} options={{ headerShown: false }} />
